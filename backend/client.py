48dbe6a6158fe2bafac3781d4e40f8cab7b90509
Saurabh Atreya <atreyasaurabh@gmail.com>
2024-08-29 23:57:04 +0530

    feat: handle excess llm output

--- backend/client.py
+++ backend/client.py
@@ -1,5 +1,7 @@
+import re
+import json
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 from utils.env import GROQ_API_KEY, GROQ_CHAT_MODEL
 from utils.io import read_file
@@ -25,16 +27,21 @@ def generate_questions(form_data, user_language):
             break
     return questions
 
+def extract_json(S):
+    json_match = re.search(r'\{.*\}', S, re.DOTALL)
+    json_str = json_match.group(0)
+    return json.loads(json_str)
+
 parsing_prompt = PromptTemplate(
     template=read_file("static/prompts/process_response.txt"),
     input_variables=["message", "language"],
 )
-parsing = parsing_prompt | chat | JsonOutputParser()
+parsing = parsing_prompt | chat | StrOutputParser()
 
 def parse_message(message, user_language):
     prompt_input = {"message": message, "language": user_language}
     try:
-        returned_dict = parsing.invoke(prompt_input)
+        returned_dict = extract_json(parsing.invoke(prompt_input))
     except Exception as e:
         print(f"Error while parsing message: {e}")
         return dict()
@@ -44,12 +51,12 @@ image_parsing_prompt = PromptTemplate(
     template=read_file("static/prompts/process_image.txt"),
     input_variables=["string", "language"],
 )
-image_parsing = image_parsing_prompt | chat | JsonOutputParser()
+image_parsing = image_parsing_prompt | chat | StrOutputParser()
 
 def parse_image(string, user_language):
     prompt_input = {"string": string, "language": user_language}
     try:
-        returned_dict = image_parsing.invoke(prompt_input)
+        returned_dict = extract_json(image_parsing.invoke(prompt_input))
     except Exception as e:
         print(f"Error while parsing image: {e}")
         return dict()
